7d1fdf28d1b36e486e9c64ecc5af8f19752016a0
awatchanaman <94679765+awatchanaman@users.noreply.github.com>
2023-02-13 14:47:15 +0100

    Correction install uninstall

--- apprenti-geometre/tools/chocolateyinstall.ps1
+++ apprenti-geometre/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   softwareName   = 'apprenti-geometre*'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url            = 'https://github.com/awatchanaman/Apprenti-Geometre/blob/main/apprenti-geometre/tools/install_geometre.exe'
+  url            = 'https://github.com/awatchanaman/Apprenti-Geometre/raw/main/apprenti-geometre/tools/install_geometre.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   checksum       = '108C772787C3A2AF5189B61ACF132E6A08DAA4285B6BBAB26D81C6B77E415851'
@@ -14,3 +14,14 @@ $packageArgs = @{
 
 
 Install-ChocolateyPackage @packageArgs
+
+# Mise en place du Raccourci dans Menu Demarrer
+$filepath = Join-Path "$env:ProgramFiles\Apprenti-Geometre" 'Apprenti_Geometre.exe'
+Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Apprenti_Geometre.lnk" -TargetPath "$filepath"
+
+
+# Mise en place du Raccourci dans Menu Demarrer (si le params est renseigné --params /DesktopShortcut)
+
+if ((Get-PackageParameters).DesktopShortcut) {
+Install-ChocolateyShortcut -ShortcutFilePath "$env:Public\Desktop\Apprenti_Geometre.lnk" -TargetPath "$filepath"
+}
