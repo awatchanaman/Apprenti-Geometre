6431f4b48c90f9e83b714217b437fe675120d96a
awatchanaman <94679765+awatchanaman@users.noreply.github.com>
2023-02-14 09:22:59 +0100

    modif install.ps1 zip

--- apprenti-geometre/tools/chocolateyinstall2.ps1
+++ apprenti-geometre/tools/chocolateyinstall2.ps1
@@ -3,31 +3,26 @@
 $unzipLocation = "$env:TEMP\$packagename"
 
 $packageArgs = @{
-  packageName   = $packagename
+  packageName   = 'apprenti-geometre'
   unzipLocation = $unzipLocation
-  Url           = "https://download.tuxfamily.org/makehuman/releases/makehuman-community-1.2.0-windows.zip"
+  Url           = "https://github.com/awatchanaman/Apprenti-Geometre/raw/main/apprenti-geometre/install_geometre.zip"
   checksum      = '018B5EB2E6A6DD0A3C5246E1ACD3CE53F2950BC9E930E46FD2343C065E11E194'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
 
-
-
-
 $packageArgs = @{
   packageName    = 'apprenti-geometre'
   softwareName   = 'apprenti-geometre*'
-  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url            = 'https://github.com/awatchanaman/Apprenti-Geometre/raw/main/apprenti-geometre/install_geometre.zip'
+  file          = "$unzipLocation\install_geometre.exe"
   silentArgs     = '/S'
   validExitCodes = @(0)
   checksum       = '108C772787C3A2AF5189B61ACF132E6A08DAA4285B6BBAB26D81C6B77E415851'
   checksumType   = 'sha256'
   }
 
-Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
 # Mise en place du Raccourci dans Menu Demarrer
 $filepath = Join-Path "$env:ProgramFiles\Apprenti-Geometre" 'Apprenti_Geometre.exe'
