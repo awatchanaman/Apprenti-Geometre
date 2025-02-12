537b1670caffb53ea9eef8403416bdcb8ffdc811
awatchanaman <94679765+awatchanaman@users.noreply.github.com>
2025-02-12 14:35:09 +0100

    modif installation

--- apprenti-geometre/tools/chocolateyinstall.ps1
+++ apprenti-geometre/tools/chocolateyinstall.ps1
@@ -1,10 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Url64      = "https://firebasestorage.googleapis.com/v0/b/project-359443804751131057.appspot.com/o/logiciels%2FAG%2FAG%202.6.9%20(x%2064).exe?alt=media&token=780f02bd-8c16-4697-84d2-e18ac4fd418b"
-  checksum      = '54F3BBD91F84C3448590AD3238DA50F105D9F0BF3C969242F865D32C28DC0143'
-  checksumType  = 'sha256'
-}
+$Url64      = "https://firebasestorage.googleapis.com/v0/b/project-359443804751131057.appspot.com/o/logiciels%2FAG%2FAG%202.6.9%20(x%2064).exe?alt=media&token=780f02bd-8c16-4697-84d2-e18ac4fd418b"
+$uninstall = "C:\Program Files (x86)\Apprenti Géomètre 2\unins000.exe"
 
 
 $packageArgs = @{
@@ -14,42 +12,20 @@ $packageArgs = @{
   softwareName  = 'apprenti-geometre*'
   checksum64      = '54F3BBD91F84C3448590AD3238DA50F105D9F0BF3C969242F865D32C28DC0143'
   checksumType64   = 'sha256'
-  silentArgs     = '/S'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
   }
 
+# Desinstallation de la version precedente
+if (Test-path -Path "C:\Program Files (x86)\Apprenti Géomètre 2\unins000.exe") {
+  Write-Host "Desinstallation de la version precedente"
+  Start-Process -FilePath $uninstall -ArgumentList "-arg1 /VERYSILENT -arg2 /SUPPRESSMSGBOXES -arg3 /NORESTART -arg4 /SP-" -NoNewWindow -Wait
+}
+
 Install-ChocolateyPackage @packageArgs
 
-#$packageArgs = @{
- 
-
-  # MSI
-  #silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  #validExitCodes= @(0, 3010, 1641)
-  # OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  #silentArgs   = '/S'           # NSIS
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s'           # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://community.chocolatey.org/packages/autohotkey.portable
-
-
-
-
-# Mise en place du Raccourci dans Menu Demarrer
-#$filepath = Join-Path "$env:ProgramFiles\Apprenti-Geometre" 'Apprenti_Geometre.exe'
-#Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Apprenti_Geometre.lnk" -TargetPath "$filepath"
-
-
-# Mise en place du Raccourci dans Menu Demarrer (si le params est renseigné --params /DesktopShortcut)
-
-#if ((Get-PackageParameters).DesktopShortcut) {
-#Install-ChocolateyShortcut -ShortcutFilePath "$env:Public\Desktop\Apprenti_Geometre.lnk" -TargetPath "$filepath"
-#}
+# Mise en place du Raccourci bureau
+if (Test-path -Path "C:\Users\Public\Desktop\Mathematiques") {
+  $filepath = Join-Path "C:\Program Files (x86)\Apprenti Géomètre 2" 'AG2.exe'
+  Install-ChocolateyShortcut -ShortcutFilePath "C:\Users\Public\Desktop\Mathematiques\Apprenti Géomètre 2.lnk" -TargetPath "$filepath"
+}
