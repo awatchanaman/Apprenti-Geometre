76077cace5bea494f7404f01495772a1cfc56b12
awatchanaman <94679765+awatchanaman@users.noreply.github.com>
2025-02-12 11:39:53 +0100

    modif version

--- apprenti-geometre/tools/chocolateyinstall.ps1
+++ apprenti-geometre/tools/chocolateyinstall.ps1
@@ -1,36 +1,55 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$unzipLocation = "$env:TEMP\$packagename"
-
-$packageArgs = @{
-  packageName   = 'apprenti-geometre'
-  unzipLocation = $unzipLocation
-  Url           = "https://github.com/awatchanaman/Apprenti-Geometre/raw/main/apprenti-geometre/install_geometre.zip"
-  checksum      = '018B5EB2E6A6DD0A3C5246E1ACD3CE53F2950BC9E930E46FD2343C065E11E194'
+$toolsDir  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Url64      = "https://firebasestorage.googleapis.com/v0/b/project-359443804751131057.appspot.com/o/logiciels%2FAG%2FAG%202.6.9%20(x%2064).exe?alt=media&token=780f02bd-8c16-4697-84d2-e18ac4fd418b"
+  checksum      = '54F3BBD91F84C3448590AD3238DA50F105D9F0BF3C969242F865D32C28DC0143'
   checksumType  = 'sha256'
 }
-Install-ChocolateyZipPackage @packageArgs
+
 
 $packageArgs = @{
-  packageName    = 'apprenti-geometre'
-  softwareName   = 'apprenti-geometre*'
-  fileType       = 'exe'
-  file          = "$unzipLocation\install_geometre.exe"
+  packageName   = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url64bit      = $url64
+  softwareName  = 'apprenti-geometre*'
+  checksum64      = '54F3BBD91F84C3448590AD3238DA50F105D9F0BF3C969242F865D32C28DC0143'
+  checksumType64   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
-  checksum       = '108C772787C3A2AF5189B61ACF132E6A08DAA4285B6BBAB26D81C6B77E415851'
-  checksumType   = 'sha256'
   }
 
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
+
+#$packageArgs = @{
+ 
+
+  # MSI
+  #silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  #validExitCodes= @(0, 3010, 1641)
+  # OTHERS
+  # Uncomment matching EXE type (sorted by most to least common)
+  #silentArgs   = '/S'           # NSIS
+  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  #silentArgs   = '/s'           # InstallShield
+  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
+  #silentArgs   = '/s'           # Wise InstallMaster
+  #silentArgs   = '-s'           # Squirrel
+  #silentArgs   = '-q'           # Install4j
+  #silentArgs   = '-s'           # Ghost
+  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
+                                 #       https://community.chocolatey.org/packages/autohotkey.portable
+
+
+
 
 # Mise en place du Raccourci dans Menu Demarrer
-$filepath = Join-Path "$env:ProgramFiles\Apprenti-Geometre" 'Apprenti_Geometre.exe'
-Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Apprenti_Geometre.lnk" -TargetPath "$filepath"
+#$filepath = Join-Path "$env:ProgramFiles\Apprenti-Geometre" 'Apprenti_Geometre.exe'
+#Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Apprenti_Geometre.lnk" -TargetPath "$filepath"
 
 
 # Mise en place du Raccourci dans Menu Demarrer (si le params est renseigné --params /DesktopShortcut)
 
-if ((Get-PackageParameters).DesktopShortcut) {
-Install-ChocolateyShortcut -ShortcutFilePath "$env:Public\Desktop\Apprenti_Geometre.lnk" -TargetPath "$filepath"
-}
+#if ((Get-PackageParameters).DesktopShortcut) {
+#Install-ChocolateyShortcut -ShortcutFilePath "$env:Public\Desktop\Apprenti_Geometre.lnk" -TargetPath "$filepath"
+#}
